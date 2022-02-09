107915e4304463a6156583a8cf73027a8a8e095b
rsk8262 <rohitskhanolkar@gmail.com>
2022-02-09 15:08:07 -0600

    Coded Slingshot/Projectile

---
+++ MissionDemolition-Unity/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,99 @@
+/****
+* Created by; Rohit Khanolkar
+* Date Created: 2/9/2022
+* 
+* Last Edited by: NA
+* Last edited: 2/9/2022
+* 
+
+****/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    /** Variables **/
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+    public float velocityMultiplier = 8f;
+
+
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos; //launch position of projectile
+    public GameObject projectile; //instance of projectile
+    public bool aimingMode; //is player aiming
+    public Rigidbody projectileRB; //RigidBody of projectile
+
+
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint"); //find child transform
+
+        launchPoint = launchPointTrans.gameObject; //the game object of of the child object
+        launchPoint.SetActive(false); //disable game object
+        launchPos = launchPointTrans.position;
+    }
+
+    private void Update()
+    {
+        if (!aimingMode) return;
+
+        //get the current mouse position in 2d screen coordinates
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPos; //pixel amount of change between the mouse3D and launchPos
+
+        //limit mouseDelta to slingshot collider radius
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize(); //sets the vector to the same direction, but length is 1
+            mouseDelta *= maxMagnitude;
+        }
+
+        //move projectile to new position
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos; 
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projectileRB.isKinematic = false;
+            projectileRB.velocity = -mouseDelta * velocityMultiplier;
+            projectile = null; //forget last instance
+        }
+
+    }
+
+
+
+    private void OnMouseEnter()
+    {
+        launchPoint.SetActive(true);
+        print("Slingshot: OnMouseEnter");
+    }
+
+    private void OnMouseExit()
+    {
+        launchPoint.SetActive(false); 
+        print("Slingshot: OnMouseExit");
+    }
+
+    private void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectileRB = projectile.GetComponent<Rigidbody>();
+        projectileRB.isKinematic = true;
+
+    }
+
+
+
+}
